7601c0d66d8e2004f650e84e1bc02d76d0555ad3
Mattie43 <ericksenm93@ymail.com>
2020-08-11 13:24:47 -0400

    done? cant figure out errors

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 class Triangle
   # write code here
+  attr_accessor :side1, :side2, :side3
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if side1 == side2 && side2 == side3 && side3 == side1
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side3 == side1
+      :isosceles
+      
+      # how is side1 <= 0 not working???
+    elsif side1 <= 0 || side2 <= 0 || side3 <= 0 || (side1 + side2) < side3 || (side2 + side3) < side1 || (side1 + side3) < side2
+      raise TriangleError
+    else
+      :scalene
+    end
+  end
+
+      class TriangleError < StandardError
+      end
+
 end
